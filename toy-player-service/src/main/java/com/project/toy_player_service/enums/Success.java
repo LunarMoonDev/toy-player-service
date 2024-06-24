1407be56b1ef1f528b6b270733adfb92ba8d6dc5
Harjit Brar <workandresearchbrar@gmail.com>
2024-06-24 16:18:10 +0800

    feat: added new success for player delete api

--- toy-player-service/src/main/java/com/project/toy_player_service/enums/Success.java
+++ toy-player-service/src/main/java/com/project/toy_player_service/enums/Success.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Success {
-    PLAYER_CREATE_SUCCESS("PLYR-SUCC-01", "Player has been create.");
+    PLAYER_CREATE_SUCCESS("PLYR-SUCC-01", "Player has been create."),
+    PLAYER_DELETE_SUCCESS("PLYR-SUCC-02", "Given total shows the number of deleted players");
     
     private final String code;
     private final String message;
