8aee7a2070f65e7d0c189413a1885792e74f493f
Harjit Brar <workandresearchbrar@gmail.com>
2024-06-24 16:21:09 +0800

    feat: added endpoint to player delete api

--- toy-player-service/src/main/java/com/project/toy_player_service/controller/v1/PlayerController.java
+++ toy-player-service/src/main/java/com/project/toy_player_service/controller/v1/PlayerController.java
@@ -4,19 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.toy_player_service.dto.player.request.PlayerDeleteRequestDTO;
 import com.project.toy_player_service.dto.player.request.PlayerRequestDTO;
+import com.project.toy_player_service.dto.player.response.PlayerDeleteResponseDTO;
 import com.project.toy_player_service.dto.player.response.PlayerResponseDTO;
 import com.project.toy_player_service.exceptions.GenericException;
 import com.project.toy_player_service.service.PlayerService;
 
 import jakarta.validation.Valid;
 import reactor.core.publisher.Mono;
-
 
 @RestController("PlayerControllerV1")
 @RequestMapping("/v1.0/player")
@@ -25,10 +28,15 @@ public class PlayerController {
     @Qualifier("PlayerService_STABLE")
     private PlayerService service;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<ResponseEntity<PlayerResponseDTO>> postPlayer(@RequestHeader("X-Tracker") String uuid,
-            @RequestBody @Valid PlayerRequestDTO payload) throws GenericException{
+            @RequestBody @Valid PlayerRequestDTO payload) throws GenericException {
         return service.createPlayer(uuid, payload).map(ResponseEntity::ok);
+    }
+
+    @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<PlayerDeleteResponseDTO>> deletePlayers(@RequestHeader("X-Tracker") String uuid,
+            @RequestBody @Valid PlayerDeleteRequestDTO payload) throws GenericException {
+        return service.deletePlayers(uuid, payload).map(ResponseEntity::ok);
     }
 }
