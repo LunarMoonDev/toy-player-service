2fa62750bb00b57c16bd9d2caeab2b3f3e4be938
Harjit Brar <workandresearchbrar@gmail.com>
2024-06-24 16:19:23 +0800

    feat: added service to delete players

--- toy-player-service/src/main/java/com/project/toy_player_service/service/PlayerService.java
+++ toy-player-service/src/main/java/com/project/toy_player_service/service/PlayerService.java
@@ -1,6 +1,8 @@
 package com.project.toy_player_service.service;
 
+import com.project.toy_player_service.dto.player.request.PlayerDeleteRequestDTO;
 import com.project.toy_player_service.dto.player.request.PlayerRequestDTO;
+import com.project.toy_player_service.dto.player.response.PlayerDeleteResponseDTO;
 import com.project.toy_player_service.dto.player.response.PlayerResponseDTO;
 import com.project.toy_player_service.exceptions.GenericException;
 
@@ -8,4 +10,6 @@ import reactor.core.publisher.Mono;
 
 public interface PlayerService {
     Mono<PlayerResponseDTO> createPlayer(String uuid, PlayerRequestDTO payload) throws GenericException;
+
+    Mono<PlayerDeleteResponseDTO> deletePlayers(String uuid, PlayerDeleteRequestDTO payload) throws GenericException;
 }
